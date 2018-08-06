73628c7a9994b147edb1e5b1fafda373ff00f330
kirya <cloudkserg11@gmail.com>
2018-08-06 08:36:45 +0300

    https://chronobank.atlassian.net/browse/MD-551 - middleware waves rest - fixed profileModel opportunity to use separate connections

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ const config = require('./config'),
 
 mongoose.Promise = Promise;
 mongoose.accounts = mongoose.createConnection(config.mongo.accounts.uri);
+mongoose.profile = mongoose.createConnection(config.mongo.profile.uri);
 
 if (config.mongo.data.useData)
   mongoose.data = mongoose.createConnection(config.mongo.data.uri);
 
-_.chain([mongoose.accounts, mongoose.data])
+_.chain([mongoose.accounts, mongoose.data, mongoose.profile])
   .compact().forEach(connection =>
     connection.on('disconnected', function () {
       log.error('mongo disconnected!');
